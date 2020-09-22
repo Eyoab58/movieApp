ee02f158bc27dba01541336797a8d1ce49effb0d
Emma Paris <eyoabasrat@gmail.com>
2020-09-21 20:58:07 -0400

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Eyoab Asrat on 9/21/20.
+//  Copyright © 2020 Eyoab58. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
