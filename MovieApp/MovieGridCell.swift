3bd0d9f2025d7b7c1eb95579f951299d93f14aab
Emma Paris <eyoabasrat@gmail.com>
2020-10-02 20:01:45 -0400

    Assignment 2

---
+++ MovieApp/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  MovieApp
+//
+//  Created by Eyoab Asrat on 10/2/20.
+//  Copyright © 2020 Eyoab58. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
